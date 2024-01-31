f5104bcb9b3a48a46c6ee96e8a482d8ae5bff32b
Anthony Green <green@moxielogic.org>
2024-01-31 10:31:24 -0500

    Handle db failure

--- gb/alerts-panel.lisp
+++ gb/alerts-panel.lisp
@@ -1,6 +1,6 @@
 ;;; -*- Mode: LISP; Syntax: COMMON-LISP; Base: 10 -*-
 ;;;
-;;; Copyright (C) 2020, 2021, 2022  Anthony Green <green@moxielogic.com>
+;;; Copyright (C) 2020, 2021, 2022, 2024  Anthony Green <green@moxielogic.com>
 ;;;
 ;;; gb is free software; you can redistribute it
 ;;; and/or modify it under the terms of the GNU General Public License
@@ -23,38 +23,45 @@
    (content :initform "")
    (query :reader query)))
 
+(defun connect-to-db ((alerts-panel alerts-panel))
+  (unless (slot-value alerts-panel 'db-connect)
+    (log:info (getenv "TIMESCALEDB_HOST"))
+    (log:info (getenv "TIMESCALEDB_PASSWORD"))
+    (handler-case
+        (progn
+          (log:info "Trying to connect...")
+          (setf (slot-value alerts-panel 'db-connection)
+                (let* ((db-host (getenv "TIMESCALEDB_HOST"))
+                       (dbc (dbi:connect-cached :postgres :database-name "gdash"
+                                                :host db-host
+                                                :port 5432
+                                                :username "gdash" :password (getenv "TIMESCALEDB_PASSWORD"))))
+                  (log:info "Connected to timescaledb at ~A:5432 (~A)" db-host dbc)
+                  (dolist (cmd '("CREATE TABLE IF NOT EXISTS tower_notifications (name character(40), status character(10), url character(128), unixtimestamp integer);"
+                                 "CREATE TABLE IF NOT EXISTS alerts (message character(128), unixtimestamp integer);"))
+                    (dbi:do-sql dbc cmd))
+                  (setf (slot-value alerts-panel 'query)
+                        (dbi:prepare (db-connection alerts-panel)
+                                     "select message, unixtimestamp from alerts order by unixtimestamp desc limit 10;"))
+                  dbc)))
+      (error (e)
+             (log:error "Error connecting to database" e)
+             nil))))
+
 (defmethod initialize-instance :after ((alerts-panel alerts-panel) &key)
   (log:info "initialize-instance alerts-panel")
-  (log:info (getenv "TIMESCALEDB_HOST"))
-  (log:info (getenv "TIMESCALEDB_PASSWORD"))
-  (log:info "About to connect...")
-  (setf (slot-value alerts-panel 'db-connection)
-        (let* ((db-host (getenv "TIMESCALEDB_HOST"))
-               (dbc (dbi:connect-cached :postgres :database-name "gdash"
-                                                  :host db-host
-                                                  :port 5432
-                                                  :username "gdash" :password (getenv "TIMESCALEDB_PASSWORD"))))
-          (log:info "Connected to timescaledb at ~A:5432 (~A)" db-host dbc)
-
-          (dolist (cmd '("CREATE TABLE IF NOT EXISTS tower_notifications (name character(40), status character(10), url character(128), unixtimestamp integer);"
-                         "CREATE TABLE IF NOT EXISTS alerts (message character(128), unixtimestamp integer);"))
-            (dbi:do-sql dbc cmd))
-
-          dbc))
-
-  (setf (slot-value alerts-panel 'query)
-        (dbi:prepare (db-connection alerts-panel)
-                     "select message, unixtimestamp from alerts order by unixtimestamp desc limit 10;"))
+  (connect-to-db alerts-panel)
   (log:info "finished initialize-instance alerts-panel")
   alerts-panel)
 
 (defmethod process ((alerts-panel alerts-panel) data)
   (log:info "PROCESS ~A" data)
-  (let ((message (cl-base64:base64-string-to-string (sb-ext:octets-to-string data))))
-    (dbi:do-sql (slot-value alerts-panel 'db-connection)
-      (format nil "insert into alerts(message, unixtimestamp) values ('~A', round(extract(epoch from now())));"
-	      (cl-ppcre:regex-replace-all "'" message "''"))))
-  (pull-alerts-log))
+  (when (connect-to-db alerts-panel)
+    (let ((message (cl-base64:base64-string-to-string (sb-ext:octets-to-string data))))
+      (dbi:do-sql (slot-value alerts-panel 'db-connection)
+                  (format nil "insert into alerts(message, unixtimestamp) values ('~A', round(extract(epoch from now())));"
+	                        (cl-ppcre:regex-replace-all "'" message "''"))))
+    (pull-alerts-log)))
 
 (defun-push push-alerts (alerts-html) (+ajax-pusher+)
   ((@ console log) "push-alerts")
@@ -80,27 +87,28 @@
                                     "<thead><tr><th>Time</th><th>Message</th></tr></thead>"
                                     *alerts-html*))
                  ((@ ($ window) trigger) "resize")))
-	  (update-alerts)
-	  (set-interval update-alerts 5000))))
+	        (update-alerts)
+	        (set-interval update-alerts 5000))))
     (update-alerts-panel)))
 
 (defun pull-alerts-log ()
-  (let* ((result (dbi:execute (query *alerts-panel*)))
-	 (fstr (make-array '(0) :element-type 'base-char
-                                :fill-pointer 0 :adjustable t)))
-    (log:info "pull-alerts-log!")
-    (with-output-to-string (s fstr)
-      (loop for row = (dbi:fetch result)
-            while row
-            do (destructuring-bind (j1 message j2 unixtimestamp)
-                   row
-                 (format s "<tr><td>~A</td><td>~A</td></tr>"
-                         (local-time:format-timestring
-                          nil
-                          (local-time:unix-to-timestamp unixtimestamp)
-                          :format local-time:+asctime-format+)
-                         message)))
-      (log:info fstr)
-      (setf (slot-value *alerts-panel* 'content) fstr)
-      (let ((hunchentoot:*acceptor* (acceptor *alerts-panel*)))
-        (push-alerts fstr)))))
+  (when (connect-to-db alerts-panel)
+    (let* ((result (dbi:execute (query *alerts-panel*)))
+	         (fstr (make-array '(0) :element-type 'base-char
+                             :fill-pointer 0 :adjustable t)))
+      (log:info "pull-alerts-log!")
+      (with-output-to-string (s fstr)
+        (loop for row = (dbi:fetch result)
+              while row
+              do (destructuring-bind (j1 message j2 unixtimestamp)
+                     row
+                   (format s "<tr><td>~A</td><td>~A</td></tr>"
+                           (local-time:format-timestring
+                            nil
+                            (local-time:unix-to-timestamp unixtimestamp)
+                            :format local-time:+asctime-format+)
+                           message)))
+        (log:info fstr)
+        (setf (slot-value *alerts-panel* 'content) fstr)
+        (let ((hunchentoot:*acceptor* (acceptor *alerts-panel*)))
+          (push-alerts fstr))))))
